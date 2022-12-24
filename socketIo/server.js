88247f34af7773314bbb36cc36109d837cb5cc4c
Rajeev <rajeevkr8917@gmail.com>
2022-12-25 05:03:18 +0530

    basic connection of socket.io added

---
+++ socketIo/server.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const express = require('express')
+const app = express()
+const socketio = require('socket.io')
+
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+    console.log('Connection with socket id: ', socket.id)
+
+    socket.on('msg_send', (data) => {
+        io.emit('msg_reci', data)
+    })
+})
+
+app.use('/', express.static(__dirname + '/public'))
+
+server.listen(4444, () => {
+    console.log('Server started on http://localhost:4444')
+})
+
